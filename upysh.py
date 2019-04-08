8c5ae42ea1cd6140ed2b628fdcbe0a0e4e15cbfb
hh <robert@hammelrath.com>
2019-04-08 18:08:07 +0200

    upysh.py: Adapt to the most recent version of the firmware

--- upysh.py
+++ upysh.py
@@ -7,10 +7,31 @@ class LS:
         self.__call__()
         return ""
 
-    def __call__(self, path="/"):
-        l = os.ls(path)
-        for f in l:
-            print(f)
+    def __call__(self, path=""):
+        for l in os.ilistdir(path):
+            if l[1] & 0x4000:  # stat.S_IFDIR
+                print("   <dir> %s" % (l[0]))
+            else:
+                print("%8d %s" % (l[3], l[0]))
+
+class PWD:
+
+    def __repr__(self):
+        res = os.getcwd()
+        if res == "": # TLD on esp8266
+            res = "/"
+        return res
+
+    def __call__(self):
+        return self.__repr__()
+
+class CLEAR:
+
+    def __repr__(self):
+        return "\x1b[2J\x1b[H"
+
+    def __call__(self):
+        return self.__repr__()
 
 class MAN:
 
@@ -18,13 +39,17 @@ class MAN:
         return ("""
 This is the 'upysh' command list:
 
-ls
 cat(file)
-hd(file)
+clear
+cd(new_dir)
+ls
+ls(object)
 head(file [, #_of_lines])
 man
+mkdir(newdir)
 mv(from, to)
 newfile(file)
+pwd
 rm(file)
 rmdir(empty_dir)
 """)
@@ -33,62 +58,33 @@ rmdir(empty_dir)
         return self.__repr__()
 
 
+pwd = PWD()
 ls = LS()
+clear = CLEAR()
 man = MAN()
+cd = os.chdir
+mkdir = os.mkdir
 mv = os.rename
 rm = os.remove
-rmdir = os.remove
+rmdir = os.rmdir
 
-def head(name, count=10):
-    f = open(name)
-    l = f.read().split("\n")
-    n = 0
-    for _ in l:
-        print(_)
-        n += 1
-        if n >= count:
-            break
-    f.close()
+def head(f, n=10):
+    with open(f) as f:
+        for i in range(n):
+            l = f.readline()
+            if not l: break
+            sys.stdout.write(l)
 
 def cat(f):
     head(f, 1 << 30)
 
-#
-# Hexdump Function
-#
-FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
-
-def hexdump(src):
-    N = 0
-    result = ''
-    length = 16
-    while src:  # loop through string
-        s,src = src[:length],src[length:]
-        if type(s) is str:
-            hexa = ' '.join(["{:02x}".format(ord(_)) for _ in s])
-            s = ''.join([FILTER[ord(_)] for _ in s])
-        else:
-            hexa = ' '.join(["{:02x}".format(_) for _ in s])
-            s = ''.join([FILTER[_] for _ in s])
-        print("{:04x}   {:48s} {:s}".format(N, hexa, s))
-        N += length
-
-def hd(name):
-    f = open(name)
-    l = f.read()
-    f.close()
-    hexdump(l)
-
 def newfile(path):
     print("Type file contents line by line, finish with EOF (Ctrl+D).")
-    f = open(path, "w")
-    data = ""
-    while 1:
-        try:
-            l = input()
-        except EOFError:
-            break
-        data += l + '\n'
-    f.write(data)
-    f.close()
-
+    with open(path, "w") as f:
+        while 1:
+            try:
+                l = input()
+            except EOFError:
+                break
+            f.write(l)
+            f.write("\n")
